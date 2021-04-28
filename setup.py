172c800f93e5379c728bfb06dac3b00a6f8a209b
severilov <severilov.pa@phystech.edu>
2021-04-28 09:53:57 +0300

    add setup instructions

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.2',
+    description='last version of houselib',
+    author='Pavel Severilov',
+    license='MIT',
+)
